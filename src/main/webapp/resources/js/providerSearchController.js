b56d7eb502f8f6867f77257d195bbff3892097cd
prashanth <prasjaga@in.ibm.com>
2016-12-16 12:51:30 +0530

    Google Map And Viciinity Based search and Look and feel Improvement

--- src/main/webapp/resources/js/providerSearchController.js
+++ src/main/webapp/resources/js/providerSearchController.js
@@ -4,15 +4,28 @@
   //Provider Search Controller function which handles the https calls and search result display for the application
   var providerSearchController = function($scope, $http, $location, $anchorScroll, $timeout) {
 
+    var mapCenter = {
+      center: new google.maps.LatLng(36.0078, -98.0929),
+      zoom: 6,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapCenter);
+
+    var markers = [];
+
     //function which fetches the search results from the database through HTTP method call 
     $scope.providerSearch = function(searchData) {
       validateForm();
       $scope.displayResult = false;
       $scope.emptyResult = false;
-      if (!($scope.invalidData)) {
+      $scope.showMap = false;
+      if (!($scope.invalidData || $scope.missingData)) {
+    	  setTransWindowVisiblitily('visible');
         $http.post('searchProvider', $scope.searchData).then(function(response) {
           if (response.data.length > 0) {
             $scope.getSearchData = response.data;
+            createMarker($scope.getSearchData);
             $scope.sortBy = $scope.sortByFields[0];
             $scope.sortSign = false;
             $scope.displayResult = true;
@@ -21,14 +34,65 @@
           } else {
             $scope.emptyResult = true;
           }
+          setTransWindowVisiblitily('hidden');
         });
-
       }
     };
+
+    function setTransWindowVisiblitily(visibility){
+    	document.getElementById('transWindow').style.visibility = visibility;
+    }
+    
+    function createMarker(dataList) {
+      deleteMarkers();
+      for (var i = 0; i < dataList.length; i++) {
+        addMarker(dataList[i]);
+      }
+    }
+
+    function addMarker(data) {
+      var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(data.address.latitude, data.address.longitude),
+        title: data.name,
+        map: map
+      });
+
+      var infowindow = new google.maps.InfoWindow({
+        content: '<div class="fontHeading padSpace">' + data.name +
+          '</div><div class="fontSmall"><strong>Type:</strong>' + data.providerType.description +
+          '<br/><strong>County:</strong>' + data.address.county +
+          '<br/><strong>City:</strong>' + data.address.city +
+          '<br/><strong>Capacity:</strong>' + data.capacity +
+          '<br/><strong>Phone#:</strong>' + data.phoneNumber + '</div>'
+      });
+
+      marker.addListener('click', function() {
+        map.setCenter(marker.position);
+        map.setZoom(9);
+        infowindow.open(map, marker);
+      });
+
+      markers.push(marker);
+    }
+
+    // Removes the markers from the map, but keeps them in the array.
+    function clearMarkers() {
+      for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(null);
+      }
+    }
+
+    // Deletes all markers in the array by removing references to them.
+    function deleteMarkers() {
+      clearMarkers();
+      markers = [];
+    }
 
     //function to validate the form data
     function validateForm() {
       $scope.invalidData = true;
+      $scope.missingData = false;
+      $scope.showDistance = false;
       if ($scope.searchData) {
         var formValues = [];
         if ($scope.searchData.providerName) {
@@ -39,6 +103,13 @@
         }
         if ($scope.searchData.city) {
           formValues.push($scope.searchData.city);
+        }
+        if ($scope.searchData.geoRadius) {
+          if (!($scope.searchData.city)) {
+            $scope.missingData = true;
+          }
+          $scope.showDistance = true;
+          formValues.push($scope.searchData.geoRadius.description);
         }
         if ($scope.searchData.county) {
           formValues.push($scope.searchData.county);
@@ -54,6 +125,13 @@
       }
     }
 
+    //function which shows or hides the map
+    $scope.toggleShowMap = function() {
+      $scope.showMap = !$scope.showMap;
+      $timeout(function() {
+        google.maps.event.trigger(map, 'resize');
+      });
+    };
 
 
     //function which sorts the search result in asc or desc order
@@ -83,6 +161,20 @@
       tempCity = response.data;
     };
 
+    $scope.geoRadius = [{
+      radius: 10,
+      description: "10 mile radius"
+    }, {
+      radius: 25,
+      description: "25 mile radius"
+    }, {
+      radius: 50,
+      description: "50 mile radius"
+    }, {
+      radius: 100,
+      description: "100 mile radius"
+    }];
+
     var loadQRating = function(response) {
       $scope.qualityRating = response.data;
     };
@@ -100,7 +192,7 @@
         $anchorScroll();
         $location.hash(id);
       });
-    };
+    }
 
     //function which resets the data in the form
     $scope.resetData = function() {
@@ -110,6 +202,8 @@
       $scope.city = tempCity;
       $scope.emptyResult = false;
       $scope.displayResult = false;
+      $scope.missingData = false;
+      $scope.showMap = false;
     };
 
     //block which fetches the search form display data from the database
@@ -122,6 +216,9 @@
 
     //variable which gives the list of option to the sort feature
     $scope.sortByFields = [{
+      name: "Distance",
+      value: "address.distance"
+    }, {
       name: "Provider Name",
       value: "name"
     }, {
